d975968d44a8bac3010c5612110067efcc1db76a
mymac <trybook1901@gmail.com>
2025-02-09 14:56:39 +0900

    ユーザーログインAPI、JWT認証機能追加

--- api/server.js
+++ api/server.js
@@ -2,6 +2,8 @@ const { PrismaClient } = require("@prisma/client");
 const express = require("express");
 const app = express();
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 const PORT = 8000;
 
@@ -26,6 +28,33 @@ app.post("/api/auth/register", async (req, res) => {
     },
   });
   return res.json(user);
+});
+
+// ユーザーログインAPI
+app.post("/api/auth/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (!user) {
+    return res.status(401).json({ message: "ユーザーが見つかりません。" });
+  }
+
+  const isValidPassword = await bcrypt.compare(password, user.password);
+
+  if (!isValidPassword) {
+    return res.status(401).json({ message: "パスワードが間違っています。" });
+  }
+
+  const token = jwt.sign({ userId: user.id }, process.env.SECRET_KEY, {
+    expiresIn: "1d",
+  });
+
+  return res.json(token);
 });
 
 app.listen(PORT, () => console.log(`サーバー起動中 Port: ${PORT}`));
